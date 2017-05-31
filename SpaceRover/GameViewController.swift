67c5bcfeaecf559149f1e788d8709bb4bc981f2c
Hazen O'Malley <Hazen@hazens-air.home>
2017-05-30 21:28:55 -0700

    add pan

--- SpaceRover/GameViewController.swift
+++ SpaceRover/GameViewController.swift
@@ -11,6 +11,9 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+  @IBAction func doPan(_ sender: UIPanGestureRecognizer) {
+    print("do pan")
+  }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,21 +27,21 @@ class GameViewController: UIViewController {
             skView.ignoresSiblingOrder = true
             
             /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+            scene.scaleMode = .aspectFill
             
             skView.presentScene(scene)
         }
     }
 
-    override func shouldAutorotate() -> Bool {
+    override var shouldAutorotate : Bool {
         return true
     }
 
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
         } else {
-            return .All
+            return .all
         }
     }
 
@@ -47,7 +50,7 @@ class GameViewController: UIViewController {
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override func prefersStatusBarHidden() -> Bool {
+    override var prefersStatusBarHidden : Bool {
         return true
     }
 }
